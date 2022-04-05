9a30ef69b3dfe5c1929ffe42c311483b72d8d5b6
nandakishore2610 <kasula.n@northeastern.edu>
2022-04-04 22:20:56 -0400

    testcases

---
+++ src/tests/dislike-screen.test.js
@@ -0,0 +1,26 @@
+import { act } from 'react-dom/test-utils';
+import Profile from "../components/profile/index"
+import MyDislikes from "../components/profile/my-dislikes"
+import {screen, render} from "@testing-library/react";
+import {HashRouter} from "react-router-dom";
+
+const MOCKED_USER = {username: "bob", _id: "234"};
+const MOCKED_TUIT =
+    [{tuit: "alice's tuit", postBy: "234", _id: "2341", stats: {likes: 25, dislikes: 6}}];
+
+const mockAxios = jest.genMockFromModule('axios')
+mockAxios.create = jest.fn(() => mockAxios)
+
+test('renders dislikes tab on profile', async() => {
+    mockAxios.get.mockImplementation(() =>
+        Promise.resolve({ data: MOCKED_USER }));
+
+    await act( async () => render(
+        <HashRouter>
+            <Profile />
+        </HashRouter>
+    ));
+
+    const dislikeTab = screen.getByText(/Dislikes/i);
+    expect(dislikeTab).toBeInTheDocument();
+})
